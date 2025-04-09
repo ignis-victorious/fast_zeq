83d42d2230638f4b5c788005222f43edab87a574
Emmanuele Levi <ignisvictorious@gmail.com>
2025-04-09 16:54:25 +0100

    Explore validation options

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #  __________________
 #  Import LIBARIES
-from fastapi import FastAPI
+from typing import Any
+from fastapi import FastAPI, Path
 from enum import Enum
 #  Import FILES
 #  __________________
@@ -40,19 +41,50 @@ def index_delete() -> dict[str, str]:
     return {"message": "Delete request"}
 
 
+ITEM_ID_VALIDATION = Path(..., title="The string ID of the Item", max_length=5)
+ITEM_AMOUNT_VALIDATION = Path(..., title="The number of item to get", ge=1, le=99)
 # @app.get("/items/2")
 # async def get_item_2() -> dict[str, int]:
 #     return {"message": "Invalid"}
 
 
-@app.get("/items/{item_id}")
-async def get_item(item_id: int) -> dict[str, int]:
-    return {"Hello": item_id}
+# Path(
+#     ...,
+#     alias="for",
+#     title="A title i want to give",
+#     description="Something that describe the above",
+#     # Number Specific validation
+#     gt=0,  # >0 - ge=1 #>=1, lt = 11 #<11, le = 10 #<=10, #multiple_of=3, #3,6,9,...
+#     # String Specific validation
+#     min_length=1,
+#     max_length=10,
+#     pattern="[a-zAz]+",  # regex="[a-zAz]+",
+#     # Options
+#     strict=True,
+#     examples=["3", "6"],
+#     deprecated=True,
+#     # Others
+# ),
+@app.get("/items/{item_id}/{amount}")
+async def get_item(
+    item_id: str = ITEM_ID_VALIDATION, amount: int = ITEM_AMOUNT_VALIDATION
+) -> dict[str, Any]:
+    return {"Itam ID": item_id, "amount": amount}
 
 
-@app.get("/items/2")
-async def get_item_2() -> dict[str, int]:
-    return {"message": "Invalid"}
+# @app.get("/items/{item_id}")
+# async def get_item(item_id: str = ITEM_ID_VALIDATION) -> dict[str, str]:
+#     return {"Hello": item_id}
+
+
+# @app.get("/items/{item_id}")
+# async def get_item(item_id: int) -> dict[str, int]:
+#     return {"Hello": item_id}
+
+
+# @app.get("/items/2")
+# async def get_item_2() -> dict[str, int]:
+#     return {"message": "Invalid"}
 
 
 @app.get("/fruits/{fruit}/{amount}")
