d7b281375008781cbd4bd3854be3d3b5b8fb4593
Emmanuele Levi <ignisvictorious@gmail.com>
2025-04-09 15:27:37 +0100

    path order or with enums or two or more param

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 #  __________________
 #  Import LIBARIES
 from fastapi import FastAPI
+from enum import Enum
 #  Import FILES
 #  __________________
 
 
-app = FastAPI()
+class Fruit(Enum):
+    apple = "apple"
+    banana = "banana"
+    orange = "orange"
+
+
+app = FastAPI(
+    title="FastAPI Tutorial",
+    description="This is a pretty simple tutorial",
+    summary="Not sure what this is!!!",
+    version="1.0.0",
+)
 
 
 @app.get("/")
@@ -26,6 +38,30 @@ def index_put() -> dict[str, str]:
 @app.delete("/")
 def index_delete() -> dict[str, str]:
     return {"message": "Delete request"}
+
+
+# @app.get("/items/2")
+# async def get_item_2() -> dict[str, int]:
+#     return {"message": "Invalid"}
+
+
+@app.get("/items/{item_id}")
+async def get_item(item_id: int) -> dict[str, int]:
+    return {"Hello": item_id}
+
+
+@app.get("/items/2")
+async def get_item_2() -> dict[str, int]:
+    return {"message": "Invalid"}
+
+
+@app.get("/fruits/{fruit}/{amount}")
+async def get_fruit(fruit: Fruit, amount: int | None) -> dict[str, str]:
+    if amount > 1:
+        return {"fruit": f"{fruit.value}s =  {amount}"}
+    if amount == 1:
+        return {"fruit": f"{fruit.value} =  1"}
+    return {"fruit": f"{fruit.value}"}
 
 
 # def main():
